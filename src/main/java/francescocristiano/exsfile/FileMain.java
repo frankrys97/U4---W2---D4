11fe7ed14dd05739cc6aacb3b5235a652d46a31d
Francesco Cristiano <frankrys97@gmail.com>
2024-05-30 16:08:18 +0200

    ex 6 fixes

--- src/main/java/francescocristiano/exsfile/FileMain.java
+++ src/main/java/francescocristiano/exsfile/FileMain.java
@@ -4,6 +4,8 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
 
@@ -16,22 +18,36 @@ public class FileMain {
 
         Supplier<product.Product> productSupplier = () -> new product.Product(enums.Category.values()[random.nextInt(enums.Category.values().length)], product.Product.generateRandomPrice());
 
-        salvaProdottiSuDisco(productSupplier.get(), file);
-        salvaProdottiSuDisco(productSupplier.get(), file);
-        salvaProdottiSuDisco(productSupplier.get(), file);
-        salvaProdottiSuDisco(productSupplier.get(), file);
-        salvaProdottiSuDisco(productSupplier.get(), file);
+
+        List<product.Product> products1 = new ArrayList<>();
+        List<product.Product> products2 = new ArrayList<>();
+        List<product.Product> products3 = new ArrayList<>();
+        List<product.Product> products4 = new ArrayList<>();
+        List<product.Product> products5 = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            products1.add(productSupplier.get());
+            products2.add(productSupplier.get());
+            products3.add(productSupplier.get());
+            products4.add(productSupplier.get());
+            products5.add(productSupplier.get());
+        }
+
+        salvaProdottiSuDisco(products1, file);
+        salvaProdottiSuDisco(products2, file);
+        salvaProdottiSuDisco(products3, file);
+        salvaProdottiSuDisco(products4, file);
+        salvaProdottiSuDisco(products5, file);
 
 
         leggiProdottiDaDisco(file);
 
-
     }
 
 
-    public static void salvaProdottiSuDisco(product.Product product, File file) {
+    public static void salvaProdottiSuDisco(List<product.Product> products, File file) {
         try {
-            FileUtils.writeStringToFile(file, product.toString() + System.lineSeparator(), "UTF-8", true);
+            FileUtils.writeStringToFile(file, products.toString() + System.lineSeparator(), "UTF-8", true);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
